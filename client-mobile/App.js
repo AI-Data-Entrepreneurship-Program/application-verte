c93cf9765109672a9e2a8bcd63dcfc9893f96d1e
Fabien <fabien-luc.lallemand@epitech.eu>
2021-04-28 23:44:08 +0400

    remove usless files

--- client-mobile/App.js
+++ client-mobile/App.js
@@ -1,17 +1,14 @@
 import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import ThemeContextProvider from './contexts/Theme';
 import StackNavigator from './navigation/StackNavigator';
 
 const App = () => {
     return (
         <SafeAreaProvider>
-            <ThemeContextProvider>
-                <NavigationContainer>
-                    <StackNavigator />
-                </NavigationContainer>
-            </ThemeContextProvider>
+            <NavigationContainer>
+                <StackNavigator />
+            </NavigationContainer>
         </SafeAreaProvider>
     );
 };
