9313b7099368489cd7a4307da41086b77b61e20b
Fabien <fabien-luc.lallemand@epitech.eu>
2021-05-15 22:04:47 +0400

    add styling to bottom tab

--- client-mobile/src/navigations/BottomTabNavigator.js
+++ client-mobile/src/navigations/BottomTabNavigator.js
@@ -1,5 +1,9 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import React from 'react';
+import { StyleSheet } from 'react-native';
+import { default as EntypoIcon } from 'react-native-vector-icons/Entypo';
+import { default as FontAwesome5Icon } from 'react-native-vector-icons/FontAwesome5';
+import { colors } from '../consts/styles';
 import HomeScreen from '../screens/Home';
 import RankingScreen from '../screens/Ranking';
 
@@ -7,12 +11,56 @@ const BottomTabNavigator = createBottomTabNavigator();
 
 export default function BottomTabNavigatorProvider() {
     return (
-        <BottomTabNavigator.Navigator>
-            <BottomTabNavigator.Screen name='Home' component={HomeScreen} />
+        <BottomTabNavigator.Navigator
+            tabBarOptions={{
+                showLabel: false,
+                style: styles.container
+            }}
+        >
+            <BottomTabNavigator.Screen
+                name='Home'
+                component={HomeScreen}
+                options={{
+                    tabBarIcon: ({ focused }) => (
+                        <EntypoIcon
+                            name='home'
+                            size={24}
+                            color={
+                                focused ? colors.darkGreen : colors.lightGreen
+                            }
+                        />
+                    )
+                }}
+            />
             <BottomTabNavigator.Screen
                 name='Ranking'
                 component={RankingScreen}
+                options={{
+                    tabBarIcon: ({ focused }) => (
+                        <FontAwesome5Icon
+                            name='medal'
+                            size={24}
+                            color={
+                                focused ? colors.darkGreen : colors.lightGreen
+                            }
+                        />
+                    )
+                }}
             />
         </BottomTabNavigator.Navigator>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        position: 'absolute',
+        width: '40%',
+        height: 50,
+        bottom: 25,
+        left: '30%',
+        right: '30%',
+        backgroundColor: '#ffffff',
+        borderRadius: 20,
+        elevation: 0
+    }
+});
