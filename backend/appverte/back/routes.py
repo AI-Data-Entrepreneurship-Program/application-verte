75d7e5b86a65cb96cce7a6cff445f93b192969b7
RomainBaud26 <rbaud26@gmail.com>
2021-05-18 20:39:27 +0200

    add comments section for action

--- backend/appverte/back/routes.py
+++ backend/appverte/back/routes.py
@@ -8,7 +8,7 @@ back = Blueprint('back', __name__)
 api = Api(back)
 
 
-from appverte.back.ressources import Cartes, Users, Badge, Likes, Favourite, EarnBadge
+from appverte.back.ressources import Cartes, Users, Badge, Likes, Favourite, EarnBadge, Comment
 
 api.add_resource(Cartes, '/api/cartes', '/api/cartes/<carte_id>')  
 api.add_resource(Users, '/api/users', '/api/users/<user_id>') 
@@ -16,4 +16,5 @@ api.add_resource(Badge, '/api/badges', '/api/badges/<badge_id>')
 api.add_resource(Likes, '/api/likes') 
 api.add_resource(Favourite, '/api/favourite') 
 api.add_resource(EarnBadge, '/api/earnBadge') 
+api.add_resource(Comment, '/api/comment') 
 
--- backend/main.py
+++ backend/main.py
@@ -28,12 +28,67 @@ def setup_database(app):
                 description= value['action_description'],
                 image_url= value['action_image'],
                 impact= value['action_impact'],
-                category= value['category'],
+                category= "{}".format([i.strip() for i in value['category'].split(",")]),
                 #source = value['sources'],
                 rating = 0,
                 liked_by = '{}'.format([]),
                 disliked_by = '{}'.format([]),
-                top_action = 0
+                top_action = 0,
+                comments = '{}'.format({
+    'uuid': {
+        'user_id':'xxx', 
+        'avatar_url':'https://www.w3schools.com/w3images/avatar6.png',
+        'username':'Marc',
+        'content':"Cette action est super. Je conseille de mettre un peu plus d'huile d'olive :)",
+        'likes_count':5,
+        'dislikes_count':95,
+        'answers': {
+            'uuid': {
+                'user_id':'xxx', 
+                'avatar_url':'https://content.invisioncic.com/v286142/monthly_2020_07/large.png.5eeb4e7900429d4fbea8789a3194c030.png',
+                'username':'Martin',
+                'content':"J'adore, j'ai essayé et c'est parfait avec 50cl de plus.",
+                'likes_count':4,
+                'dislikes_count':0
+                },
+            'uuid': {
+                'user_id':'xxx', 
+                'avatar_url':'https://content.invisioncic.com/v286142/monthly_2020_07/large.png.5eeb4e7900429d4fbea8789a3194c030.png',
+                'username':'Martin',
+                'content':"Enfin j'ai aussi ajouté un peu plus de sel.",
+                'likes_count':0,
+                'dislikes_count':0
+                },
+            'uuid': {
+                'user_id':'xxx', 
+                'avatar_url':'https://content.invisioncic.com/v286142/monthly_2020_07/large.png.5eeb4e7900429d4fbea8789a3194c030.png',
+                'username':'Martin',
+                'content':"Mais c'est vraiment une bonne idée",
+                'likes_count':0,
+                'dislikes_count':0
+                }
+        } 
+    },
+    'uuid2': {
+        'user_id':'xxxxx', 
+        'avatar_url':'https://www.w3schools.com/w3images/avatar5.png',
+        'username':'Marc',
+        'content':"Attention c'est du fake !!!!",
+        'likes_count':152,
+        'dislikes_count':4,
+        'answers': {
+            'uuid': {
+                'user_id':'xxx', 
+                'avatar_url':'https://www.w3schools.com/w3images/avatar4.png',
+                'username':'Martin',
+                'content':"Ca marche pour moi. ",
+                'likes_count':4,
+                'dislikes_count':0
+                }
+
+        } 
+    }
+}),
             )
             )
         db.session.commit()
