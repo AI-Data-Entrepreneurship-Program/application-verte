87366398148b0f7032a6d2307cf155fa8f61f5ef
Fabien <fabien-luc.lallemand@epitech.eu>
2021-06-22 11:39:58 +0400

    remove unused files

--- client-mobile/src/components/BackgroundCurve/styles.js
+++
@@ -1,9 +0,0 @@
-import { StyleSheet } from 'react-native';
-
-const styles = StyleSheet.create({
-    container: {
-        zIndex: -1
-    }
-});
-
-export default styles;
--- client-mobile/src/navigations/StackNavigator.js
+++ client-mobile/src/navigations/StackNavigator.js
@@ -1,11 +1,8 @@
 import { createStackNavigator } from '@react-navigation/stack';
 import React from 'react';
 import CardDetails from '../screens/CardDetailsExp';
-import FormScreen from '../screens/Form';
 import HomeScreen from '../screens/Home';
-import LoginScreen from '../screens/Login';
 import ProfileScreen from '../screens/Profile';
-import RegisterScreen from '../screens/Register';
 import SplashScreen from '../screens/Splash';
 
 const StackNavigator = createStackNavigator();
@@ -21,21 +18,6 @@ export default function StackNavigatorProvider() {
             <StackNavigator.Screen
                 name='CardDetails'
                 component={CardDetails}
-                options={{ headerShown: false }}
-            />
-            <StackNavigator.Screen
-                name='Login'
-                component={LoginScreen}
-                options={{ headerShown: false }}
-            />
-            <StackNavigator.Screen
-                name='Register'
-                component={RegisterScreen}
-                options={{ headerShown: false }}
-            />
-            <StackNavigator.Screen
-                name='Form'
-                component={FormScreen}
                 options={{ headerShown: false }}
             />
             <StackNavigator.Screen
